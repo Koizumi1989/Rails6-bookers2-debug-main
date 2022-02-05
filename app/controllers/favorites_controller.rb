5ae25d677d5bd0143678c20050fedadc23f657bb
Koizumi1989 <08033431635a@gmail.com>
2022-02-05 12:55:15 +0000

    [いいね非同期残り、books/show,users/show]

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,22 +1,24 @@
 class FavoritesController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
+    favorite = Favorite.new(book_id:@book.id,user_id:current_user.id)
+    favorite.save
+    # redirect_to request.referer
+  end
+    # paramsはurlから取ってくる。rails routesのurlを確認しておなじにする。
+    # rails routesみれば[:book_id]がurl部分にでてくる。またこのbook_idはカラムではなくurl
     # favorite = current_user.favorites.new(book_id: book.id)
     # ここでひつようなのbook_idとuser_idが必要。favoriteテーブルの中のカラム。
     # favorite = current_user.favorites.new(book_id: book.id)と下記記述は同じ内容
-    favorite = Favorite.new(book_id:book.id,user_id:current_user.id)
     # (左側はカラムの名前：右が代入する値（book=Book.find(params[:book_id])のこと)
     # (左側はカラムの名前：右が代入する値（current_user.id)のこと)
-    favorite.save
-    redirect_to request.referer
-  end
 
   def destroy
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id: @book.id)
     favorite.destroy
-    redirect_to request.referer
+    # redirect_to request.referer
   end
 
 end
