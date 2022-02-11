96ea91137be3977798fc435b06a83fd96eb44ee9
Koizumi1989 <08033431635a@gmail.com>
2022-02-11 05:39:58 +0000

    [gest_user_Finish]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   before_action :ensure_correct_user, only: [:edit,:update]
+  # ゲストユーザー　コントローラーeditアクションが実行される前の処理
+  before_action :ensure_guest_user, only: [:edit]
 
   def show
     @book = Book.new
@@ -30,6 +32,14 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :introduction, :profile_image)
   end
+  
+  # ゲストユーザー ユーザーの編集画面のurlを直接打たれた時の対応。
+  def ensure_guest_user
+    @user = User.find(params[:id])
+    if @user.name == "guestuser"
+      redirect_to user_path(current_user) , notice: 'ゲストユーザーはプロフィール編集画面へ遷移できません。'
+    end
+  end  
 
   def ensure_correct_user
     @user = User.find(params[:id])
