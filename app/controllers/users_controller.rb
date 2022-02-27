b71fc2cc483cdc840351741d880288a439a8e83a
Koizumi1989 <08033431635a@gmail.com>
2022-02-27 10:09:19 +0000

    8bFinish

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,14 +8,28 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @books = @user.books.includes(:favorited_users).sort {|a,b| b.favorited_users.size <=> a.favorited_users.size}
     @books_user =@user.books
+    # 投稿者数関連
     @today_book = @books_user.created_today
     @yesterday_book = @books_user.created_yesterday
+    @book_2day = @books_user.created_2day
     @book_3day = @books_user.created_3day
     @book_4day = @books_user.created_4day
     @book_5day = @books_user.created_5day
     @book_6day = @books_user.created_6day
     @thisweek_book = @books_user.created_thisweek
     @lastweek_book = @books_user.created_lastweek
+  end
+
+  def search
+    @user = User.find(params[:user_id])
+    @books = @user.books
+    @book = Book.new
+    if params[:created_at] == ""
+      @search_book = "日付を選択してください"
+    else
+      create_at = params[:created_at]
+      @search_book = @books.where(['created_at LIKE ? ', "#{create_at}%"]).count
+    end
   end
 
   def index
